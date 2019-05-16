a76603ebdb3adf38c953452cd259aa675b729418
halostormls <2524416894@qq.com>
2019-05-16 12:26:37 +0800

    remove .so copy

--- tools/install_annotation.sh
+++ tools/install_annotation.sh
@@ -1,9 +1,5 @@
 cd $pwd ..
 
-sudo cp src/rviz_cloud_annotation/lib/librviz_cloud_annotation_com.so  /usr/lib/librviz_cloud_annotation_com.so
-
-sudo cp src/rviz_cloud_annotation/lib/librviz_cloud_annotation_plugin.so  /usr/lib/librviz_cloud_annotation_plugin.so
-
 catkin_make
 
 source devel/setup.bash
